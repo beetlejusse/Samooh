5cfb45176f08613da1cbb46408cadbf751de0df2
sogeking-nul <utsav.jana.5@gmail.com>
2025-05-08 17:04:14 +0530

    added favicon

--- app/layout.tsx
+++ app/layout.tsx
@@ -8,8 +8,12 @@ import { SessionProvider } from "next-auth/react"
 const inter = Inter({ subsets: ["latin"] })
 
 export const metadata: Metadata = {
-  title: "Samooh - College Event Community",
-  description: "Discover, list, and manage college events such as hackathons, workshops, and tech talks.",
+  title: "Samooh - College Event Aggregator",
+  description: "Discover, List, and Manage College Events such as hackathons, workshops, and tech talks.",
+  icons: {
+    icon: "/images/favicon.ico",
+    apple: '/images/favicon.ico'
+  },
 }
 
 export default function RootLayout({
